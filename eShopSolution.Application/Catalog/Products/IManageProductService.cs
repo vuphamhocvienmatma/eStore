d376d3ac218d562fd4a985bf9e15a82e47e04426
Vu Pham <vuphamhocvienmatma@gmail.com>
2020-10-07 13:57:12 +0700

    Add project files.

---
+++ eShopSolution.Application/Catalog/Products/IManageProductService.cs
@@ -0,0 +1,38 @@
+﻿using eShopSolution.ViewModels.Catalog.ProductImages;
+using eShopSolution.ViewModels.Catalog.Products;
+using eShopSolution.ViewModels.Common;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    public interface IManageProductService
+    {
+        Task<int> Create(ProductCreateRequest request);
+
+        Task<int> Update(ProductUpdateRequest request);
+
+        Task<int> Delete(int productId);
+
+        Task<ProductViewModel> GetById(int productId, string languageId);
+
+        Task<bool> UpdatePrice(int productId, decimal newPrice);
+
+        Task<bool> UpdateStock(int productId, int addedQuantity);
+
+        Task AddViewcount(int productId);
+
+        Task<PagedResult<ProductViewModel>> GetAllPaging(GetManageProductPagingRequest request);
+
+        Task<int> AddImage(int productId, ProductImageCreateRequest request);
+
+        Task<int> RemoveImage(int imageId);
+
+        Task<int> UpdateImage(int imageId, ProductImageUpdateRequest request);
+
+        Task<ProductImageViewModel> GetImageById(int imageId);
+
+        Task<List<ProductImageViewModel>> GetListImages(int productId);
+    }
+}
